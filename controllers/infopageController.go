f7533dd3938d8f981f13fc07e1e2679f0f8d26f6
yxiliu <yxiliu@yeah.net>
2020-08-16 20:16:46 +0800

    f

--- controllers/infopageController.go
+++ controllers/infopageController.go
@@ -13,12 +13,12 @@ import (
 func IndxRecordTable(c *gin.Context) {
 	var id string = c.DefaultQuery("id", "0")
 	var year string = c.DefaultQuery("year", "2020")
-	idint, _ = strconv.Atoi(id)
-	yearint, _ = strconv.Atoi(year)
+	idint, _ := strconv.Atoi(id)
+	yearint, _ := strconv.Atoi(year)
 	listofindex := models.FindAllRecordByIndexAndYear(uint(idint), uint(yearint))
 	c.JSON(http.StatusOK, gin.H{
 		"code": 2000,
-		"data": indexlist,
+		"data": listofindex,
 	})
 }
 
@@ -32,7 +32,7 @@ func IndxInfo(c *gin.Context) {
 			"data":   "no id",
 		})
 	} else {
-		idint, _ = strconv.Atoi(id)
+		idint, _ := strconv.Atoi(id)
 		var idx models.Indx
 		idx.FindOne(uint(idint))
 		c.JSON(http.StatusOK, gin.H{
